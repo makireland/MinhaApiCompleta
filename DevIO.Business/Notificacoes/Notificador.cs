c8cadd8b56adc911f365bb575ab3f611d12c90a5
Maksuel Milhomem <mmilhomem@brainworks.com>
2023-08-06 17:56:56 +0100

    Implementing notifications

--- DevIO.Business/Notificacoes/Notificador.cs
+++ DevIO.Business/Notificacoes/Notificador.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevIO.Business.Notificacoes
+﻿namespace DevIO.Business.Notificacoes
 {
-    internal class Notificador
+    public class Notificador : INotificador
     {
+        private List<Notificacao> _notificacoes;
+
+        public Notificador()
+        {
+            _notificacoes = new List<Notificacao>();
+        }
+
+        public bool TemNotificacao()
+        {
+            return _notificacoes.Any();
+        }
+
+        public void Handle(Notificacao notificacao)
+        {
+            _notificacoes.Add(notificacao);
+        }
+
+        public List<Notificacao> ObterNotificacoes()
+        {
+            return _notificacoes;
+        }
     }
 }
