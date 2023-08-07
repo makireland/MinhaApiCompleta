137684f467fab38309f5e64942d5d681ed3a8d6e
Maksuel Milhomem <mmilhomem@brainworks.com>
2023-08-07 23:33:13 +0100

    adding migration, setting connection string, injecting Interfaces and repositories by adding scoped

--- DevIO.Api/Program.cs
+++ DevIO.Api/Program.cs
@@ -1,3 +1,8 @@
+using DevIO.Business.Interfaces;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MeuDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("MinhaApiDb")));
+
+builder.Services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<IFornecedorRepository, FornecedorRepository>();
 
 var app = builder.Build();
 
